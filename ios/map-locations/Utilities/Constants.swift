6bee3926f1cd11a99d37fd04b288a71b01a3c005
Nic Laughter <nicholaslaughter@icloud.com>
2024-09-27 13:20:59 -0600

    Fixed folder location

---
+++ ios/map-locations/Utilities/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  map-locations
+//
+//  Created by Nic on 9/27/24.
+//
+
+struct Constants {
+    static let locationsDataURLString = "https://raw.githubusercontent.com/historadical/coding-exercises/master/mobile/map-locations/locations.json"
+}
